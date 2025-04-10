4f58479f8e11edccad582882f8fb7d4b25c7616b
Coder200212 <coder@glynac.ai>
2025-04-10 11:51:02 -0500

    work?

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow">
@@ -39,15 +39,13 @@ const App = () => (
               <Route path="/counseling/lead-generation" element={<CounselingPages.LeadGeneration />} />
               <Route path="/counseling/practice-management" element={<CounselingPages.PracticeManagement />} />
               <Route path="/counseling/well-being" element={<CounselingPages.WellBeing/>} />
-              {/* Removing wellness route since it doesn't exist in CounselingPages */}
               <Route path="/contact" element={<Contact />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
           <Footer />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
